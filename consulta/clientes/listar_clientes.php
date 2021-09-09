526e474a4505e1b90988714ddf34789ed26a4dea
Kayque Viana <kayque.viana@catskillet.com>
2021-09-09 16:48:25 -0300

    initial commit

---
+++ consulta/clientes/listar_clientes.php
@@ -0,0 +1,40 @@
+<section>
+
+    <h3>
+        <span>Listar Clientes</span>
+    </h3>
+</section>
+ <section class="content">
+    <div class="box">
+    <div class="box-header with-border">
+        <div class="row margin-bottom-20">
+            <div class="col-md-12 text-right">
+            <a href="Novo Cadastro" class="btn btn-sucess">Inserir Cliente</a>
+            </div>
+        </div>
+    </div>
+    </div>
+    <table class="table table-striped table-sl">
+        <thead>
+            <tr>
+                <th>NOME:</th>
+                <th class="text-center">SOBRENOME:</th>
+                <th class="text-center">EMAIL:</th>
+                <th class="text-center">TELEFONE:</th>
+                <th class="text-center">GENERO:</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>NOME:</td>
+                <td class="text-center">SOBRENOME:</td>
+                <td class="text-center">EMAIL:</td>
+                <td class="text-center">TELEFONE:</td>
+                <td class="text-center">GENERO:</td>
+            </tr>
+            <tr>
+                <td>AÇÕES:</td>
+            </tr>
+        </tbody>
+    </table>
+ </section>
