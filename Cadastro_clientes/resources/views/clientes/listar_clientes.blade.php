a1235eea84bd66e929b8d521d1ef9102f6391490
Kayque Viana <kayque.viana@catskillet.com>
2021-09-14 16:27:40 -0300

    Adicionados os arquivos do laravel, modificados os arquivos anteriores para .blade.php

---
+++ Cadastro_clientes/resources/views/clientes/listar_clientes.blade.php
@@ -0,0 +1,40 @@
+<section>
+
+    <h3>
+    <i class="fa fa-users"></i><span>Listar Clientes</span>
+    </h3>
+</section>
+ <section class="content">
+    <div class="box">
+    <div class="box-header with-border">
+        <div class="row margin-bottom-20">
+            <div class="col-md-12 text-right">
+            <a href="criarUsuario.php" title="Novo Cadastro" class="btn btn-sucess"><i class="fa fa-plus-square"></i>Inserir Cliente</a>
+            </div>
+        </div>
+    </div>
+    </div>
+    <table class="table table-striped table-sl">
+        <thead>
+            <tr>
+                <th>NOME:</th>
+                <th class="text-center">SOBRENOME:</th>
+                <th class="text-center">EMAIL:</th>
+                <th class="text-center">TELEFONE:</th>
+                <th class="text-center">GENERO:</th>
+                <th>AÇÕES:</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>NOME:</td>
+                <td class="text-center">SOBRENOME:</td>
+                <td class="text-center">EMAIL:</td>
+                <td class="text-center">TELEFONE:</td>
+                <td class="text-center">GENERO:</td>
+                
+            </tr>
+         
+        </tbody>
+    </table>
+ </section>
