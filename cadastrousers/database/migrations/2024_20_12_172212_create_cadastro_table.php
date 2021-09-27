9c04e578a6e3b63ebcbe4b4203a88d83e3a3d712
Kayque Viana <kayque.viana@catskillet.com>
2021-09-27 15:05:16 -0300

    devido a erros críticos na aplicação, iniciado um novo projeto...

---
+++ cadastrousers/database/migrations/2024_20_12_172212_create_cadastro_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCadastroTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 25);
+            $table->string('surname', 35);
+            $table->string('email', 50);
+            $table->string('cell', 35);
+            $table->boolean('gender', 3);
+            $table->string('text', 255);
+            $table->string('cep', 12);
+            $table->string('adress', 255);
+            $table->string('neighborhood', 255);
+            $table->string('city', 255);
+            $table->string('uf', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cadastro');
+    }
+}
---
+++ cadastrousers/resources/views/show.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.main')
+
+@section('title', $client->name)
+
+@section('content')
+  <div class="col-md-10 offset-md-1">
+      <div class="row">
+              <div id="info-container" class="col-md-6">
+                <h1><ion-icon name="code-working"></ion-icon>{{ $client->name }} {{ $client->surname}}</h1>
+                <p class="client-email"><ion-icon name="finger-print"></ion-icon>ID do Usuário: {{ $client->id}}</p>
+                <p class="client-email"><ion-icon name="at"></ion-icon>{{ $client->email}}</p>
+                <p class="client-email"><ion-icon name="logo-whatsapp"></ion-icon>{{ $client->cell}}</p>
+                @if( $client->gender == 0)
+                <p class="client-email"><ion-icon name="people"></ion-icon>Masculino</p>
+                @elseif( $client->gender == 1)
+                <p class="client-email"><ion-icon name="people"></ion-icon>Feminino</p>
+                @else
+                <p class="client-email"><ion-icon name="people"></ion-icon>Não Informado</p>
+                @endif
+                <p class="client-email"><ion-icon name="mail-open"></ion-icon>{{ $client->text}}</p>
+                <p class="client-email"><ion-icon name="business"></ion-icon>Cep: {{ $client->cep }} - Rua: {{ $client->adress }} - Bairro: {{ $client->neighborhood }}</p>
+                <p class="client-email"><ion-icon name="locate"></ion-icon>{{ $client->city}}-{{ $client->uf }}</p>
+              </div>
+      </div>
+  </div>
+@endsection
